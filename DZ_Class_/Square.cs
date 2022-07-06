a685b835eefbba2172ef6e0395b6851d40b96908
GinOfSand <96676512+GinOfSand@users.noreply.github.com>
2022-07-06 13:54:12 +0700

    Добавьте файлы проекта.

---
+++ DZ_Class_/Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Class_
+{
+    internal class Square
+    {
+        public int wall{ get; set; }
+        string simbol { get; set; }
+        public Square(int walls = 2, string simb = "*") { wall = walls; simbol = simb; }
+
+        public override string ToString() 
+        {
+            string sq = null; 
+            for (int i = 0; i < wall; i++)
+            {
+                for (int j = 0; j < wall; j++)
+                {
+                    sq += simbol+" ";
+                }
+                sq += "\n";
+            }
+            return sq;
+        }
+
+    }
+}
